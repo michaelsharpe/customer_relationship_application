c1a3f43f2bf79c36d750730098785a0a99dc83ee
michaelsharpe <m.sharpe@project89.org>
2013-10-24 14:24:38 -0400

    Add main menu and call option

--- crm.rb
+++ crm.rb
@@ -1,9 +1,6 @@
-class Crm
+class CRM
 
-  def initialize
-  end
-
-  def main_menu
+  def print_main_menu
     puts "\e[H\e[2J"
     puts "[1] add"
     puts "[2] modify"
@@ -12,14 +9,35 @@ class Crm
     puts "[4] display attribute"
     puts "[5] delete"
     puts "[6] exit\n"
-    puts "Please select an option:"
-    option = gets.chomp
+    puts "Please select and option:"
   end
 
-  def add
+  def main_menu
+    print_main_menu
+    user_selected = gets.to_i
+    call_option(user_selected)
   end
 
-  def modify
+  def call_option(user_selected)
+    case user_selected
+    when 1
+      add_contact
+    when 2
+      modify_contact
+    when 3
+      display_all
+    when 4
+      display_attribute
+    when 5
+      delete_contact
+    when 6
+      
+  end
+
+  def add_contact
+  end
+
+  def modify_contact
   end
 
   def display_all
@@ -31,16 +49,13 @@ class Crm
   def display_attribute
   end
 
-  def delete
+  def delete_contact
   end
 
   def exit
   end
-
 end
 
-class Contact
-end
+app = CRM.new
 
-class Rolodex
-end
+app.main_menu
---
+++ rolodex.rb
@@ -0,0 +1,2 @@
+class Rolodex
+end
